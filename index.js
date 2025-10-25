3335850d57abea61ddc2c6053b5547f872d6a411
Heitor-Grande <heitorogrande2001@gmail.com>
2025-10-25 14:37:42 -0300

    organizando projeto

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-const express = require("express")
-const dotenv = require("dotenv")
-dotenv.config()
-const path = require("path")
+const app = require("./app")
 
-const app = express()
+const porta = process.env.PORT || 3000
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-//configurando o handlebars
-const handlebars = require("express-handlebars")
-app.engine("handlebars", handlebars.engine())
-app.set("view engine", "handlebars")
-app.set("views", path.join(process.cwd(), "public/views"))
-
-// servir arquivos estáticos da pasta public
-app.use(express.static(path.join(process.cwd(), "public")))
-
-//login
-const login = require("./routes/login/login.routes")
-app.use(login)
-
-//menu principal
-const menuPrincipal = require("./routes/menuPrincipal/menuPrincipal.routes")
-app.use(menuPrincipal)
-
-//cadastro do cliente
-const routerCliente = require("./routes/cliente/cliente.routes")
-app.use(routerCliente)
-
-//servico
-const routerServico = require("./routes/servico/servico.routes")
-app.use(routerServico)
-
-//lista cliente
-const routerListaCliente = require("./routes/listaCliente/listaCliente.routes")
-app.use(routerListaCliente)
-
-module.exports = app
+app.listen(porta, function () {
+    console.log("aplicação rodando em : http://localhost:" + porta)
+})
